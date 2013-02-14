7ba6ec32b836b9b0b64cd07c7d5c300335c7e0b5
Paul Schwendenman <schwendenman.paul@gmail.com>
2013-02-14 02:08:09 -0500

    Finished the minify js script.

--- minify-js.py
+++ minify-js.py
@@ -10,15 +10,18 @@ filename = sys.argv[1].split('.')
 assert filename[1] == "js"
 
 lines = f.read()
+
 f.close()
 filename = filename[0] + '.min.js'
 
 params = urllib.urlencode([
-    ('js_code', sys.argv[1]),
-    ('compilation_level', 'WHITESPACE_ONLY'),
+    ('js_code', lines),
+    ('compilation_level', 'SIMPLE_OPTIMIZATIONS'),
     ('output_format', 'text'),
     ('output_info', 'compiled_code'),
   ])
+# 'WHITESPACE_ONLY'
+
 
 # Always use the following value for the Content-type header.
 headers = { "Content-type": "application/x-www-form-urlencoded" }
@@ -31,5 +34,4 @@ data = response.read()
 f.write(data)
 f.close()
 
-#print data
 conn.close()
