3959c13d5033d996dd9e81aa2f5d1e43eb9772bb
Paul Schwendenman <schwendenman.paul@gmail.com>
2013-02-14 12:44:19 -0500

    Improved Minify-js to make it stand alone.

--- minify-js.py
+++ minify-js.py
@@ -6,7 +6,7 @@ import httplib, urllib, sys
 # a URL-safe format.
 
 def read(filename):
-    with open(sys.argv[1], 'r') as f:
+    with open(filename, 'r') as f:
         output = f.read()
     return output
 
@@ -14,15 +14,15 @@ def write(filename, input):
     with open(filename, 'w') as f:
         f.write(input)
 
-
-def sendrecieve(input):
+def sendrecieve(input, num):
+    complier_level = ['WHITESPACE_ONLY', 'SIMPLE_OPTIMIZATIONS', 
+        'ADVANCED_OPTIMIZATIONS']
     params = urllib.urlencode([
         ('js_code', input),
-        ('compilation_level', 'SIMPLE_OPTIMIZATIONS'),
+        ('compilation_level', complier_level[num]),
         ('output_format', 'text'),
         ('output_info', 'compiled_code'),
       ])
-    # 'WHITESPACE_ONLY'
 
     # Always use the following value for the Content-type header.
     headers = { "Content-type": "application/x-www-form-urlencoded" }
@@ -33,26 +33,29 @@ def sendrecieve(input):
     conn.close()
     return data
 
-
-if __name__ == '__main__':
-    filename = sys.argv[1].split('.')
-    if filename[-1] == 'js' and len(filename) == 2:
+def main(filename, num):
+    print "%s..." % filename,
+    sys.stdout.flush()
+    filenameparts = filename.split('.')
+    if filenameparts[-1] == 'js' and len(filenameparts) == 2:
         try:
             try:
-                content = read(sys.argv[1])
-            except:
+                content = read(filename)
+            except Exception as e:
                 print "read error"
+                #print e
                 raise Exception()
             try:
-                data = sendrecieve(content)
+                data = sendrecieve(content, num)
             except Exception as e:
                 print "connection error"
+                #print e
                 raise Exception()
             try:    
-                write(filename[0] + '.min.js', data)
+                write(filenameparts[0] + '.min.js', data)
             except Exception as e:
                 print "write error"
-                print e
+                #print e
                 raise Exception()
         except:
             pass
@@ -63,5 +66,25 @@ if __name__ == '__main__':
     else:
         print "skipped"
 
+if __name__ == '__main__':
+    if sys.argv[1][:3] == '-c=' and sys.argv[1][3] in ['0', '1', '2']:
+        num = int(sys.argv[1][3])
+        for filename in sys.argv[2:]:
+            main(filename, num)
 
+    elif sys.argv[1][:17] == '--compiler-level=' and \
+         sys.argv[1][17] in ['0', '1', '2']:
+        num = int(sys.argv[1][17])
+        for filename in sys.argv[2:]:
+            main(filename, num)
+    
+    elif len(sys.argv) > 1 and sys.argv[1][0] != '-':
+        for filename in sys.argv[1:]:
+            main(filename, 0)
 
+    else:
+        print "usage: minify-js.py [options] file [file] ..."
+        print "\t-c, --compiler-level=COMPILER_LEVEL"
+        print "\t\t0: Whitespace removal only (default)"
+        print "\t\t1: Simple Optimizations"
+        print "\t\t2: Advanced Optimizations"
